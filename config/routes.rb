82228feda587c6727e87445d040bce97361ede23
hoanghung123vn <20151845@student.hust.edu.vn>
2018-09-27 15:46:14 +0700

    Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
     root "static_pages#hello"
     get "/home", to: "static_pages#home"
     get "/help", to: "static_pages#help"
-    get "contact", to: "static_pages#contact"
-    get "about", to: "static_pages#about"
-    get "signup", to: "users#new"
+    get "/contact", to: "static_pages#contact"
+    get "/about", to: "static_pages#about"
+    get "/signup", to: "users#new"
+    post "/signup",  to: "users#create"
+    resources :users
   end
 end
