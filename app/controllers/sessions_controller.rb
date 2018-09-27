248cdb4be4554646f3df6cfe9794a2a283332559
hoanghung123vn <20151845@student.hust.edu.vn>
2018-09-27 15:46:14 +0700

    Updating, showing, and deleting users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
   def create_session user
     log_in user
     params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-    redirect_to user
+    redirect_back_or user
   end
 
   def destroy
